998d6de7228efbc4ffc28d85f0dfbad9a3584901
ArtemiyAnisim <144233084+ArtemiyAnisim@users.noreply.github.com>
2023-10-11 13:28:56 +0300

    Add files via upload

---
+++ yuyu10.py
@@ -0,0 +1,6 @@
+# Больше предыдущего
+
+a = [int(i) for i in input().split()]
+for i in range(1, len(a)):
+    if a[i] > a[i - 1]:
+     print(a[i])
---
+++ yuyu5.py
@@ -0,0 +1,7 @@
+# Лесенка
+
+n = int(input())
+for i in range(1, n + 1):
+    for i in range(1, i + 1):
+        print(i, sep='', end='')
+    print()
